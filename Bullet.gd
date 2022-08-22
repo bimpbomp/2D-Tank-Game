46534f22885d452730c9b73d674bea94c89c90f3
bimpbomp <tobias.morgan97@protonmail.com>
2022-08-22 17:54:54 +0100

    end of part 4

--- Bullet.gd
+++ Bullet.gd
@@ -1,9 +1,19 @@
 extends Area2D
+class_name Bullet
 
 
 export (int) var speed = 300
 
+
+onready var killTimer = $KillTimer
+
+
 var direction := Vector2.ZERO
+
+
+func _ready():
+	killTimer.start()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -12,5 +22,11 @@ func _physics_process(delta):
 		
 		global_position += velocity
 
+
 func set_direction(direction: Vector2):
 	self.direction = direction
+	rotation += direction.angle()
+
+
+func _on_KillTimer_timeout():
+	queue_free()
--- Player.gd
+++ Player.gd
@@ -1,21 +1,25 @@
 extends KinematicBody2D
 
 
+signal player_fired_bullet(bullet, position, direction)
+
 export (int) var speed = 100
 # rotation speed in degrees (converted to radians) per second
 export (float) var rotateSpeed = 80 * PI/180
-
 export (PackedScene) var Bullet
 
 var velDirection : int = 0
 var vel := Vector2()
 var rotationVelocity : float = 0
 
+
 var readyToFire := true
+
 
 onready var tankTurret : KinematicBody2D = $Turret
 onready var turretAnimationPlayer : AnimationPlayer = $Turret/AnimationPlayer
 onready var endOfGun : Position2D = $Turret/EndOfBarrel
+onready var gunDirection : Position2D = $Turret/GunDirection
 
 
 # Called when the node enters the scene tree for the first time.
@@ -55,25 +59,25 @@ func _physics_process(delta):
 
 	else:
 		queue_free()
-		
+
+
 func _unhandled_input(event):
 	if event.is_action_pressed("fire"):
 		fire()
 
+
 func fire():
-	
+
+
 	if readyToFire:
 		# start the animation, prevent more firing until animation has finished
 		turretAnimationPlayer.play("fire")
 		readyToFire = false
 		
 		var bullet_instance = Bullet.instance()
-		get_tree().get_root().add_child(bullet_instance)
-		bullet_instance.global_position = endOfGun.global_position 
-		var direction = tankTurret.global_position.direction_to(endOfGun.global_position).normalized()
-		bullet_instance.set_direction(direction)
-		bullet_instance.rotation = direction.angle()
-		#bullet_instance.bullet_owner = bullet_owner
-	
+		var direction = endOfGun.global_position.direction_to(gunDirection.global_position).normalized()
+		emit_signal("player_fired_bullet", bullet_instance, endOfGun.global_position, direction)
+
+
 func _make_ready_to_fire(_a, _b):
 	readyToFire = true
