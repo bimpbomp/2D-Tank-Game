0ad8c87258643057ccae315d2df43d11ff9e66d0
bimpbomp <tobias.morgan97@protonmail.com>
2022-08-22 08:37:20 +0100

    end of part 2

---
+++ Player.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 100
+# rotation speed in degrees (converted to radians) per second
+export (float) var rotateSpeed = 80 * PI/180
+
+var velDirection : int = 0
+var vel := Vector2()
+var rotationVelocity : float = 0
+
+var tankBody : KinematicBody2D
+var tankTurret : KinematicBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tankBody = get_node_or_null(".")
+	tankTurret = get_node_or_null("Turret")
+
+
+func _physics_process(delta):
+	if is_instance_valid(tankBody):
+		# rotation inputs
+		rotationVelocity = 0
+		if Input.is_action_pressed("turn_left"):
+			rotationVelocity -= rotateSpeed
+		if Input.is_action_pressed("turn_right"):
+			rotationVelocity += rotateSpeed
+		
+		# apply rotation
+		tankBody.rotate(rotationVelocity * delta)
+		
+		velDirection = 0
+		# movement inputs
+		if Input.is_action_pressed("move_forwards"):
+			velDirection += 1
+		if Input.is_action_pressed("move_backwards"):
+			velDirection -= 1
+		
+		# applying velocity
+		if velDirection != 0:
+			vel = Vector2.RIGHT.rotated(tankBody.rotation) * velDirection * speed
+			vel = tankBody.move_and_slide(vel, Vector2.ZERO)
+		
+		# feel like i will need to move the actual firing code to a separate location
+		# so it can be reused for enemies also
+		if tankTurret != null:
+			tankTurret.look_at(get_global_mouse_position())
+#			if Input.is_action_pressed("fire"):
+#				tankTurret._on_fire(tankBody)
+	else:
+		queue_free()
